a922156d1400f778a1c1e99ef2def083c3f564a4
Krissy510 <56779304+Krissy510@users.noreply.github.com>
2021-11-13 16:06:32 +0700

    Update PaanDAO.java
    + boolean checkExist(String tableName)
    + int loadSettings(String type)
    +TodoList loadTodoList()

--- Kris/Test Ground/src/PaanDAO.java
+++ Kris/Test Ground/src/PaanDAO.java
@@ -21,18 +21,83 @@ public class PaanDAO {
         }
     }
 
+    // Constructor
 
     public PaanDAO() throws SQLException {
         this.connect();
         if(cnn != null) this.st = cnn.createStatement();
     }
 
-    public void updateSettings(String type, int val){
-        String sql = "UPDATE userSettings SET "+type+" = "+val+";";
+    // Check Table exist
+    public boolean checkExist(String tableName){
+        String query = "SELECT name FROM sqlite_master WHERE name='"+tableName;
+        if(tableName.equals("userSettings")) query += "'";
+        else query += "Table'";
         try{
-            st.executeUpdate(sql);
+            rs = st.executeQuery(query);
+            return rs.getString(1) != null;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    // Create Table
+    public void createTable(String tableName){
+        String command = "CREATE TABLE IF NOT EXISTS "+tableName;
+        switch (tableName) {
+            case "daily":
+            case "todo":
+                command += "Table (status INTEGER,detail TEXT);";
+                break;
+            case "event":
+                command += "Table (detail TEXT, dateTime TEXT);";
+                break;
+            case "mood":
+                command += "Table (felt INTEGER, dateTime TEXT);";
+                break;
+            case "theme":
+                command += "Table (theme INTEGER, timeFormat INTEGER);";
+                break;
+            case "water":
+                command += "Table (drink INTEGER NOT NULL);";
+                break;
+            case "userSettings":
+                command += " (theme INTEGER NOT NULL, timeFormat INTEGER NOT NULL);";
+                break;
+        }
+        try{
+            st.execute(command);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    
+
+    // Update Method
+    public void updateSettings(String type, int val){
+        String query = "UPDATE userSettings SET "+type+" = "+val+";";
+        try{
+            st.executeUpdate(query);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // Load Method
+
+    public int loadSettings(String type){
+        String query = "SELECT " + type + " FROM userSettings";
+        try {
+            int temp;
+            rs = st.executeQuery(query);
+            rs.next();
+            temp = rs.getInt(1);
+            return temp;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
         }
     }
 
@@ -55,17 +120,18 @@ public class PaanDAO {
         }
     }
 
-    public int loadSettings(String type){
-        String query = "SELECT " + type + " FROM userSettings";
+    public TodoList loadTodoList(){
+        String query = "SELECT * FROM todoTable ORDER BY status ASC";
+        TodoList temp = new TodoList();
         try {
-            int temp;
             rs = st.executeQuery(query);
-            rs.next();
-            temp = rs.getInt(1);
+            while(rs.next()){
+                temp.addTask(rs.getString(1), rs.getInt(2) == 1);
+            }
             return temp;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-            return 0;
+            return null;
         }
     }
 
